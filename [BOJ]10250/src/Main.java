1065f6b97eba6c5e97fba4356ed85c7caa0edc80
hongpoleon <milescm@nate.com>
2020-01-30 15:34:02 +0900

    Complete

--- [BOJ]10250/src/Main.java
+++ [BOJ]10250/src/Main.java
@@ -1,9 +1,31 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 0; i < N; i++) {
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int order = sc.nextInt();
+			int col = (order/H)+1;
+			int row = (order%H);
+			if(row == 0) {
+				col = col-1;
+				row = H;
+			}
+			System.out.print(row);
+			if(row >= 10) {
+				if(col < 10) {
+					System.out.print("0");
+				}
+			}
+			else {
+				if(col < 10) {
+					System.out.print("0");
+				}
+			}
+			System.out.println(col);			
+		}
 	}
-
 }
