ccd1ffabcc435fb5c1093021dbaa9b6c23206b23
hongpoleon <milescm@nate.com>
2020-03-30 21:44:35 +0900

    Correct,check again

---
+++ [BOJ]1475/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.HashMap;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		//Set plastic box
+		
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+			map.put('0', 0);
+			map.put('1', 0);
+			map.put('2', 0);
+			map.put('3', 0);
+			map.put('4', 0);
+			map.put('5', 0);
+			map.put('6', 0);
+			map.put('7', 0);
+			map.put('8', 0);
+			map.put('9', 0);
+			
+			
+			String N = sc.nextLine();
+			String[] Input = N.split("");
+			
+		
+		
+		
+
+
+
+	}
+
+}
